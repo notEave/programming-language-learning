a240ed00e7079921b37a93178b3c2969ee2f80ce
Roni Kallio <steamhoki@gmail.com>
2019-08-16 02:24:12 +0300

    Restructuring and improved mock test

--- branches/00-standard-streams-and-command-line-arguments/00-hello-world/.components/00-check.sh
+++ branches/00-standard-streams-and-command-line-arguments/00-hello-world/.components/00-check.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 
 # Fields for generating info.md file
 # Write additional notes about the test here
+# Reaching EOF causes read to exit with non-zero exit code, so wrap it in
+# set +e set -e.
 set +e
 read -r -d '' notes << EOF
 EOF
@@ -21,8 +23,6 @@ set -e
 # Expected standard output and standard error of the program
 # read -r to not interpret backslash characters, read delimits with \n by
 # default, so use -d '' to remove delimiting for multiline heredocs.
-# Reaching EOF causes read to exit with non-zero exit code, so wrap it in
-# set +e set -e.
 set +e
 read -r -d '' stdout << EOF
 Hello world
@@ -32,17 +32,39 @@ set -e
 set +e
 read -r -d '' stderr << EOF
 EOF
+set -e
+
+exitcode='0'
 
 # We want to record output into temporary files for later inspection
 stdoutfile="$(mktemp -t stdout.XXXXXX)"
 stderrfile="$(mktemp -t stderr.XXXXXX)"
 
 # Actual execution of the program, outputs redirected
+set +e
 "${@}" "${args}" > "${stdoutfile}" 2> "${stderrfile}" <<< "${stdin}"
+code="$?"
+set -e
 
+errors='0'
 # If files differ, diff's exit code will be non-zero,
 # which would cause the script to terminate when set -e is active.
 # Stop this from happening with <command> || true
-# -B to ignore blank lines
-diff --color -B -u -s "${stdoutfile}" <(echo "${stdout}") || true
-diff --color -B -u -s "${stderrfile}" <(echo "${stderr}") || true
+# -B to ignore blank lines, -u for unified output, -s reports when files match
+echo "Notes: ${notes:--}"
+
+echo 'Standard output:'
+set +e
+diff -B --color "${stdoutfile}" <(echo "${stdout}") && echo '-' || exit 1
+set -e
+
+echo 'Standard error:'
+set +e
+diff -B --color "${stderrfile}" <(echo "${stderr}") && echo '-'
+set -e
+
+
+echo 'Exit code:'
+set +e
+diff -B --color <(echo "${code}") <(echo "${exitcode}") && echo '-'
+set -e
