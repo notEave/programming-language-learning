5ca05f3c0d04267730401e08482c1cb892786256
Roni Kallio <steamhoki@gmail.com>
2019-08-16 00:13:13 +0300

    Parallelization todo

--- acts/00/0_hello_world/check.sh
+++ acts/00/0_hello_world/check.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 [ -v DEBUG ] && set -x
 
+
+# TODO Parallelize running of checks
+# for when a problem might require internet access
 for check in .components/check_[[:digit:]].sh
 do
      bash "$check" "${@}"
